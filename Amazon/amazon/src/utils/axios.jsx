12fe91879aa70a622aac6d722ad7bb153b8605ac
GokulDev7 <142340434+GokulDev7@users.noreply.github.com>
2024-02-29 18:15:48 +0530

    Delete Amazon directory

--- Amazon/amazon/src/utils/axios.jsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL:'http://127.0.0.1:5001/build-2cfa6/us-central1/api' //API (cloud function ) URL
-});
-
-export default instance;
